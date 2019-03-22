d25c499041b3f4e38aecf616071b3e003893befb
3a713240 <48341448+3a713240@users.noreply.github.com>
2019-03-22 09:46:25 +0800

    Add files via upload

---
+++ 3a713240 02.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+
+#include <stdlib.h> 
+
+
+
+int main() 
+
+{
+
+ int h,m,p;  
+
+ printf("請輸入你的出生年分(西元)：");
+
+ scanf("%d",&h);   
+
+ printf("請輸入你的出生月分：");
+
+ scanf("%d",&m);
+
+ printf("請輸入你的出生日期：");
+
+ scanf("%d",&p); 
+
+ printf("你的出生年月日為：%d,%d,%d\n",h,m,p);
+
+ system("pause");   
+
+return 0;
+
+}
+
